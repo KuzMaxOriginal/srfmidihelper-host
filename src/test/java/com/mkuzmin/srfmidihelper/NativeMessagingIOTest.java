bf89cb7aeec042831e1fc8bdba90b8f508c4dddd
MaxSeid <maxim.seid@gmail.com>
2019-02-22 07:54:31 +0300

    Added sendMessage test

--- src/test/java/com/mkuzmin/srfmidihelper/NativeMessagingIOTest.java
+++ src/test/java/com/mkuzmin/srfmidihelper/NativeMessagingIOTest.java
@@ -7,23 +7,40 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Assertions;
 
 public class NativeMessagingIOTest {
+    private final JSONObject greetingsObject = new JSONObject();
+
+    public NativeMessagingIOTest() {
+        greetingsObject.put("greetings", "hello");
+    }
+
+    private byte[] prepareByteArray(String data) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        outputStream.write(Utils.integerToByteArray(data.length(), 4));
+        outputStream.write(data.getBytes("UTF-8"));
+
+        return outputStream.toByteArray();
+    }
+
     @Test
     void receiveMessage() throws IOException {
-        JSONObject greetingsObject = new JSONObject();
-        greetingsObject.put("greetings", "hello");
-
-        String greetingsString = greetingsObject.toString();
-
-        ByteArrayOutputStream inputStreamWriter = new ByteArrayOutputStream();
-        inputStreamWriter.write(Utils.integerToByteArray(greetingsString.length(), 4));
-        inputStreamWriter.write(greetingsString.getBytes("UTF-8"));
-
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(inputStreamWriter.toByteArray());
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(prepareByteArray(greetingsObject.toString()));
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
         NativeMessagingIO nativeMessagingIO = new NativeMessagingIO(inputStream, outputStream);
 
         JSONObject receivedMessage = nativeMessagingIO.receiveMessage();
         Assertions.assertEquals("hello", receivedMessage.get("greetings"));
+    }
+
+    @Test
+    void sendMessage() throws IOException {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(new byte[0]);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        NativeMessagingIO nativeMessagingIO = new NativeMessagingIO(inputStream, outputStream);
+
+        nativeMessagingIO.sendMessage(greetingsObject);
+
+        Assertions.assertArrayEquals(prepareByteArray(greetingsObject.toString()), outputStream.toByteArray());
     }
 }
